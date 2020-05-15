b79705fcaeca484691859a503b1305e0492eafa9
Uchoosecode <awittyl44@gmail.com>
2020-05-15 16:05:04 -0400

    This should render... App Component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This should Render</h1>
+      </div>
+    )
+  }
+}
+
