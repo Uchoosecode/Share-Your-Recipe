84817e4d89e1136a71a42646d4a878783512ede6
Uchoosecode <awittyl44@gmail.com>
2020-05-15 16:06:16 -0400

    namespace api and v1

--- react-redux-backend/config/routes.rb
+++ react-redux-backend/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :chefs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :recipes
+      resources :chefs
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
+  end
+  
 end
