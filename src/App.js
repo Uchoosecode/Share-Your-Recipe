bdacf2ea6261d9ae589260944686dcaac470fc05
Uchoosecode <awittyl44@gmail.com>
2020-05-26 03:16:00 -0400

    first App.js commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
-import Search from './component/Search';
+import NavBar from './component/NavBar'
+import About from './component/About'
+import NewChef from './component/NewChef';
+import NewRecipe from './component/NewRecipe';
+import Chef from './container/ChefContainer';
+import { Switch, Route } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { fetchChefs } from './action/chefs';
+import { fetchRecipes } from './action/recipes'
 
 
-export default class App extends Component {
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchChefs()
+    this.props.fetchRecipes()
+  }
+
   render() {
     return (
-      <div>
-        <h1>This should Render</h1>
-        <Search />
-      </div>
-    )
+     <div className="App">
+      <NavBar />
+        <Switch>
+          <Route exact path="/chefs/new" component={NewChef} />       
+          <Route path="/about" component={About} />       
+        </Switch>
+     </div>
+
+    );
   }
 }
 
+export default connect(null, { fetchChefs, fetchRecipes})(App);
