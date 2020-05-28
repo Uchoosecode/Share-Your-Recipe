2a77f4726b95cb467d0d950725b0b539d02f23cc
Uchoosecode <awittyl44@gmail.com>
2020-05-28 12:37:01 -0400

    repaired fetch and create chefs

--- src/action/chefs.js
+++ src/action/chefs.js
@@ -1,40 +1,34 @@
 
 export const fetchChefs = () => {
     return (dispatch) => {
-        dispatch({ type: 'LOAD_CHEFS'})
+        dispatch({ type: 'LOAD_CHEFS'});
         fetch('http://localhost:3001/api/v1/chefs')
         .then(res => {
             return res.json()
         })
         .then(chefJSON => {
             dispatch({ type: 'ADD_CHEFS', chefs: chefJSON})
-        })
-      
+        })      
     }
 }
 
 export const createChef = (chef) => {
-    return dispatch => {
-        const body = {
-            chef
-        }
-        return("http://localhost:3001/api/v1/chefs", {
+    return (dispatch) => {
+       dispatch({ type: 'ADD_CHEFS'});
+        fetch("http://localhost:3001/api/v1/chefs", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json"
             },
-            body: JSON.stringify(body)
+            body: JSON.stringify(chef)
         })
-        .then(res => res.json())
+        .then(res => {
+            return res.json()
+        })
         .then(newChef => {
-            if (newChef.error) {
-                alert(newChef.error)
-            }
-            else {
-                dispatch()
-            }
-            return newChef
+            dispatch({type: 'ADD_CHEFS', chefs: newChef})
         })
+
     }
 }
