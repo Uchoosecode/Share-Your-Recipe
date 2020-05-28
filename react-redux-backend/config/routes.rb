609dfc487ec0c564d9dd4971f3c242c2c04d4f67
Uchoosecode <awittyl44@gmail.com>
2020-05-28 05:32:37 -0400

    added nested routes to chefs

--- react-redux-backend/config/routes.rb
+++ react-redux-backend/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :recipes
-      resources :chefs
-      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+      resources :chefs do
+        resources :recipes
+        # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+      end
     end
   end
-  
+    
 end
