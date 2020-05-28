33bccb6236e03754d21a07cd601facbde790d94a
Uchoosecode <awittyl44@gmail.com>
2020-05-28 05:35:22 -0400

    rootReducer added, index.css removed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import allReducers from './reducer/allReducers'
-import './index.css';
-import App from './App';
+import rootReducers from './reducers/rootReducers'
 import * as serviceWorker from './serviceWorker';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(allReducers, composeEnhancers(
+const store = createStore(rootReducers, composeEnhancers(
   applyMiddleware(thunk)
 ));
 
