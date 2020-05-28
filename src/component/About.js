f51615ace1d658b1c35c5bf5a5f536bda181cd8f
Uchoosecode <awittyl44@gmail.com>
2020-05-28 12:37:49 -0400

    added text and classNames

--- src/component/About.js
+++ src/component/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const About = () => (
-    <div>
-        <p>
-            This is the about Page.
+    <div className="about-page">
+        <div className="about-header">About</div>
+        <p className="about-text">
+            We welcome Chefs from all over the world and many different kinds of specialties.  We encourage you to try other recipes and see if you can add or change some of the ingredients and create a new meal.
         </p>
     </div>
 );
