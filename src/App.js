fccfe1a6a65a0c75e8ce5b47ab94c27c366b527e
Uchoosecode <awittyl44@gmail.com>
2020-05-28 05:41:58 -0400

    added Routes and import Containers

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import NavBar from './component/NavBar'
 import About from './component/About'
 import NewChef from './component/NewChef';
 import NewRecipe from './component/NewRecipe';
-import Chef from './container/ChefContainer';
+import ChefContainer from './container/ChefContainer';
+import RecipeContainer from './container/RecipeContainer'
 import { Switch, Route } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 import { fetchChefs } from './action/chefs';
-import { fetchRecipes } from './action/recipes'
+import { fetchRecipes } from './action/recipes';
 
 
 class App extends Component {
 
   componentDidMount() {
+   
     this.props.fetchChefs()
     this.props.fetchRecipes()
   }
@@ -24,8 +26,11 @@ class App extends Component {
      <div className="App">
       <NavBar />
         <Switch>
+          <Route path="/chefs" component={ChefContainer} />
           <Route exact path="/chefs/new" component={NewChef} />       
-          <Route path="/about" component={About} />       
+          <Route path="/about" component={About} /> 
+          <Route path="/recipes" component={RecipeContainer} />      
+          <Route exact path="recipes/new" component={NewRecipe} />     
         </Switch>
      </div>
 
@@ -33,4 +38,4 @@ class App extends Component {
   }
 }
 
-export default connect(null, { fetchChefs, fetchRecipes})(App);
+export default connect(null, {fetchChefs, fetchRecipes})(App);
