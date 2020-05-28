1838fa1f50d085dc0e04fbdf5c762514a783717f
Uchoosecode <awittyl44@gmail.com>
2020-05-28 12:46:42 -0400

    cleaned up component

--- src/container/ChefContainer.js
+++ src/container/ChefContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
 
 import { fetchChefs } from '../action/chefs'
 import Chefs from '../component/Chefs';
@@ -12,18 +11,13 @@ class ChefContainer extends React.Component {
         this.props.fetchChefs()
     }
     render() {
-        // debugger;
+       
     console.log(this.props.chefs)
         return(
             <div>
                 
                 <Chefs chefs={this.props.chefs}/>
-                {/* <Switch>
-                    <Route exact path="/chefs/:id" render={(routerProps)=> {
-                        const chef = chefs.find(chef => chef.id === parseInt(routerProps.match.params.id))
-                        return <Chef {...routerProps} chef={chef} />
-                    }} />
-                </Switch> */}
+                
                 ChefContainer
             </div>
         )
